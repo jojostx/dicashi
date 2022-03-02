6b6cea8d4aaaa3f2ff3e947db4fd6f4cca95a076
Ari Jay <ikuskid7@gmail.com>
2022-03-02 12:30:58 +0100

    update npm packages
    update tailwindcss to v3
    
    add fonts and logo

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ $page->language ?? 'en' }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="canonical" href="{{ $page->getUrl() }}">
-        <meta name="description" content="{{ $page->description }}">
-        <title>{{ $page->title }}</title>
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
-        <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
-    </head>
-    <body class="text-gray-900 font-sans antialiased">
-        @yield('body')
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="canonical" href="{{ $page->getUrl() }}">
+    <meta name="description" content="{{ $page->description }}">
+    <title>{{ $page->title }}</title>
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&family=Fraunces:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+    <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+</head>
+
+<body class="antialiased text-gray-900 font-fraun">
+    @yield('body')
+</body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [
-    'source/**/*.blade.php',
-    'source/**/*.md',
-    'source/**/*.html',
-  ],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        'source/**/*.blade.php',
+        'source/**/*.md',
+        'source/**/*.html',
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
+        },
+        extend: {
+            fontFamily: {
+                dm: 'DM Sans',
+                fraun: 'Fraunces',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
